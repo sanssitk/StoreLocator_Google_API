6f46cfea482963e124db6cbe4d6e6e76e518a0a0
sanjay <sanssitk@gmail.com>
2021-07-06 23:06:44 -0500

    connection with MongoDB atlas

--- exercise/app.js
+++ exercise/app.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 
 const port = process.env.port || 3000;
+const dbKey = process.env.DB_KEY
+
+// Database connection
+mongoose.connect(`mongodb+srv://sanjay_stha:${dbKey}@store-locator.jgru5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 
-app.use(express.json({limit: "5mb"}));
+// End Points
+app.use(express.json({
+    limit: "5mb"
+}));
 
 app.post("/api/stores", (req, res) => {
     let dbStores = req.body;
@@ -18,3 +30,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on Localhost:${port}`)
 })
+
